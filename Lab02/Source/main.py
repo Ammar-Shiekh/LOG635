13275d92057ecfd833e439ea580459a34a3a8ee9
psanterrep <pascal.santerre.pv4si972@gmail.com>
2015-02-18 15:51:15 -0500

    Bug Fixes + triage du texte

--- Lab02/Source/main.py
+++ Lab02/Source/main.py
@@ -1,9 +1,38 @@
-
+import unicodedata
 
 #Ouvre le fichier de l'histoire
 file = open("../twat.txt", "r")
 
-#Print le contenu du fichier
-print(file.read())
+#Array d'opération à changer
+remplacements=[[" et ",". "],[", ",". "],["qu'","que "],["n'", "ne "],["t'", "te "],["s'il", "si il "],["s'", "se "],["l'", "le "], ["m'", "me "],["j'", "je "],["d'", "de "],
+               ["c'", "cela "],["\n", ""],["-", ""],["!", "."],["?", "."],["(", ""],[")", ""],[";", ". "]]
 
-print("test")
+#Print le contenu du fichier
+texte = file.read()
+
+#Ces opérations permet d'enlever les accents
+texte = unicodedata.normalize("NFKD", texte)
+texte = texte.encode("ascii", "ignore")
+
+#Cette opération permet de mettre toute la phrase en minuscule
+texte = texte.lower()
+
+#Permet de remettre le data en string
+texte = texte.decode("utf-8")
+
+#Parcour le tableau de remplacement et effectue les remplacements
+for remplacement in remplacements:
+   texte = texte.replace(remplacement[0],remplacement[1])
+
+print(texte)
+print("clean")
+
+#grammar = grammar.FeatureGrammar.fromstring(grammaireText)
+#parser = nltk.ChartParser(grammar)
+
+#parser = parse.FeatureEarleyChartParser(grammar)
+#trees = parser.parse(texte)
+#for tree in trees:
+#    print(tree)
+#    nltk.draw.tree.draw_trees(tree)
+#    print(tree.label()['SEM'])
