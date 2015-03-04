ffcb7fa0c597c5cfb6289717a2d6fb93315f928f
psanterrep <pascal.santerre.pv4si972@gmail.com>
2015-03-04 15:44:22 -0500

    Lab 02

--- Lab02/Source/main.py
+++ Lab02/Source/main.py
@@ -1,38 +1,54 @@
 import unicodedata
+import nltk
+from nltk import *
 
 #Ouvre le fichier de l'histoire
-file = open("../twat.txt", "r")
+file = open("../texte.txt", "r")
 
+#aeiouy
 #Array d'opération à changer
-remplacements=[[" et ",". "],[", ",". "],["qu'","que "],["n'", "ne "],["t'", "te "],["s'il", "si il "],["s'", "se "],["l'", "le "], ["m'", "me "],["j'", "je "],["d'", "de "],
-               ["c'", "cela "],["\n", ""],["-", ""],["!", "."],["?", "."],["(", ""],[")", ""],[";", ". "]]
+remplacements=[["Marge ","LOL "],[",",". "],["’","'"],["'a"," a"],["'à"," à"],["'e"," e"],["'é"," é"],["'i"," i"],["'o"," o"],["'h"," h"],["'y"," y"],["'u"," u"],["\n", ""],["-", ""],["!", "."],["?", "."],["(", ""],[")", ""],[";", ". "]]
+#voir http://la-conjugaison.nouvelobs.com/regles/grammaire/les-determinants-possessifs-79.php
 
 #Print le contenu du fichier
 texte = file.read()
 
-#Ces opérations permet d'enlever les accents
-texte = unicodedata.normalize("NFKD", texte)
-texte = texte.encode("ascii", "ignore")
-
 #Cette opération permet de mettre toute la phrase en minuscule
 texte = texte.lower()
 
-#Permet de remettre le data en string
-texte = texte.decode("utf-8")
+#Ces opérations permet d'enlever les accents
+texte = unicodedata.normalize("NFKD", texte)
 
 #Parcour le tableau de remplacement et effectue les remplacements
 for remplacement in remplacements:
    texte = texte.replace(remplacement[0],remplacement[1])
 
+texte = texte.encode("ascii", "ignore")
+
+#Permet de remettre le data en string
+texte = texte.decode("utf-8")
+
+
+
+
+
+
+
+
 print(texte)
 print("clean")
 
-#grammar = grammar.FeatureGrammar.fromstring(grammaireText)
-#parser = nltk.ChartParser(grammar)
+tokens = "Lisa a mal a la tete a 3 heures".split()
 
-#parser = parse.FeatureEarleyChartParser(grammar)
-#trees = parser.parse(texte)
-#for tree in trees:
-#    print(tree)
-#    nltk.draw.tree.draw_trees(tree)
-#    print(tree.label()['SEM'])
+with open ("../grammaire/Gramm.cfg", "r") as myfile:
+    grammaireText=myfile.read()
+
+grammar = grammar.FeatureGrammar.fromstring(grammaireText)
+parser = nltk.ChartParser(grammar)
+
+parser = parse.FeatureEarleyChartParser(grammar)
+trees = parser.parse(tokens)
+for tree in trees:
+    print(tree)
+    nltk.draw.tree.draw_trees(tree)
+    print(tree.label()['SEM'])
