461377c4b396338bb8703c6fc43d9be095ee87ae
PatronDonoso <claudio.donosoo@gmail.com>
2015-03-11 14:04:44 -0400

    Jess parser, to integrate in the main.py you know!!!
    aka TU PAPA DONOSO - Miss Bum Bum ftw!!

---
+++ Lab02/Source/parserToJess.py
@@ -0,0 +1,37 @@
+﻿# -*- coding: utf-8 -*-
+
+import nltk
+from nltk import *
+
+with open ("exemple1.cfg", "r") as myfile:
+    grammaireText=myfile.read()
+
+grammar = grammar.FeatureGrammar.fromstring(grammaireText)
+parser = nltk.ChartParser(grammar)
+tokens = "Jean tua Marie".split()
+
+
+parser = parse.FeatureEarleyChartParser(grammar)
+trees = parser.parse(tokens)
+texte = 'tuer(jean,marie)is-at(homer,marie, 8)'
+for tree in trees:
+    #print(tree)
+    #nltk.draw.tree.draw_trees(tree)
+    #print(tree.label()['SEM'])
+    texte = tree.label()['SEM']
+    #print(tree.index)
+
+texte = str(texte)
+remplacements = [['(', ' '], [')', ')\n('], [',', ' ']]
+for remplacement in remplacements:
+    texte = texte.replace(remplacement[0], remplacement[1])
+
+texte = '(' + texte[:-1]
+input(texte)
+
+
+
+	
+	
+
+	
