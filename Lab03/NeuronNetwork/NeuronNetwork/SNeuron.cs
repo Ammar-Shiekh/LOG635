5c54f04c2f9a96beeb696fa7b1d18c5d4e13015d
psanterrep <pascal.santerre.pv4si972@gmail.com>
2015-03-19 17:33:49 -0400

    Première essaie de l'implémeentation du réseaux de neuronne * N'est pas complété *

---
+++ Lab03/NeuronNetwork/NeuronNetwork/SNeuron.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuronNetwork
+{
+    class SNeuron
+    {
+
+        //Nombre d'entrée dans le neuron
+        public int m_NumInputs;
+
+        //Le poids pour chaque entrée
+        public List<double> m_vectWeight;
+
+        //Contructeur d'un neuron
+        public SNeuron(int nbrInput)
+        {
+
+            for (int i = 0; i < nbrInput + 1; i++)
+            {
+                Random random = new Random();
+                //On donne un poids aléatoire entre -1 et 1
+                m_vectWeight.Add(random.NextDouble() * 2 - 1);
+            }
+        }
+    }
+}
---
+++ Lab03/NeuronNetwork/NeuronNetwork/SNeuronLayer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuronNetwork
+{
+    class SNeuronLayer
+    {
+        //le nombre de neuron dans cette couche
+        public int m_NumNeurons;
+
+        //les neurons
+        public List<SNeuron> m_vecNeurons;
+
+        public SNeuronLayer(int nbrNeurons, int nbrInputsPerNeuron)
+        {
+            for (int i = 0; i < nbrNeurons; i++)
+            {
+                this.m_vecNeurons.Add(new SNeuron(nbrInputsPerNeuron));
+            }
+        }
+
+    }
+}
